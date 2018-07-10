b932c6ec6db8d86ac9ddfcfdb31da72e9b4627e1
Alan Tsai <alan@alantsai.net>
2018-07-11 07:24:22 +0800

    加入FormFlow model

---
+++ src/MHAT.HotelBot/Models/RoomReservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MHAT.HotelBot.Models
+{
+    public enum BedSizeOptions
+    {
+        King = 1,
+        Queen,
+        Single
+    }
+
+    [Serializable]
+    public class RoomReservation
+    {
+        public DateTime StartDate { get; set; }
+        public int NumberOfNightToStay { get; set; }
+        public int NumberOfOccupants { get; set; }
+        public BedSizeOptions BedSize { get; set; }
+    }
+}

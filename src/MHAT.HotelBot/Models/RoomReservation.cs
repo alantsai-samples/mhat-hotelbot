9611a715b3d1bdf8928009056a30e12899b18803
Alan Tsai <alan@alantsai.net>
2018-07-12 20:22:28 +0800

    增加特殊邏輯處理

--- src/MHAT.HotelBot/Models/RoomReservation.cs
+++ src/MHAT.HotelBot/Models/RoomReservation.cs
@@ -30,6 +30,21 @@ namespace MHAT.HotelBot.Models
                 .Message
                 ("歡迎使用訂房功能。接下來將會問您一系列問題好讓我們幫您找到最好的房間。" +
                     $"{Environment.NewLine}有任何問題隨時打入Help將有幫助文字出現。")
+                .Field(nameof(StartDate),
+                        validate: async (state, value) =>
+                        {
+                            var result = new ValidateResult
+                                { IsValid = true, Value = value };
+
+                            var datetime = (DateTime)value;
+
+                            result.Value = datetime.AddDays(1);
+
+                            return result;
+                        })
+                  .Field(nameof(NumberOfNightToStay))
+                  .Field(nameof(NumberOfOccupants))
+                  .Field(nameof(BedSize))
                 .Build();
         }
     }

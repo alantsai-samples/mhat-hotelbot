d7c95801093450edc5dea4f656f2626a71675765
Alan Tsai <alan@alantsai.net>
2018-07-14 23:05:38 +0800

    加入訂房dialog

---
+++ src/MHAT.HotelBot/Dialogs/ReserveRoomDialog.cs
@@ -0,0 +1,19 @@
+﻿using MHAT.HotelBot.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MHAT.HotelBot.Dialogs
+{
+    [Serializable]
+    public class ReserveRoomDialog : IDialog<RoomReservation>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

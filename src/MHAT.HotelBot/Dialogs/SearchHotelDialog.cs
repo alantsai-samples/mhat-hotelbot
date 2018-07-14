c81ae6782b8737b08410a9ba2f75a425a61da772
Alan Tsai <alan@alantsai.net>
2018-07-14 22:21:47 +0800

    加入SearchHotelDialog

---
+++ src/MHAT.HotelBot/Dialogs/SearchHotelDialog.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MHAT.HotelBot.Dialogs
+{
+    [Serializable]
+    public class SearchHotelDialog : IDialog<HeroCard>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

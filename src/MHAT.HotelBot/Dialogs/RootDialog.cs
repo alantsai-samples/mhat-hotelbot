2eb449d31a11d7fdc6eac16038a4d798e9a450c0
Alan Tsai <alan@alantsai.net>
2018-07-14 22:43:21 +0800

    吧邏輯放入到idialog

--- src/MHAT.HotelBot/Dialogs/RootDialog.cs
+++ src/MHAT.HotelBot/Dialogs/RootDialog.cs
@@ -46,36 +46,12 @@ namespace MHAT.HotelBot.Dialogs
                 }
                 else if(activity.Text == "訂房")
                 {
-                    var returnMessage = activity.CreateReply();
+                    //var returnMessage = activity.CreateReply();
 
-                    var receiptCard = new ReceiptCard()
-                    {
-                        Title = "訂房費用",
-                        Total = "NT$ 120",
-                        Tax = "NT$ 20",
-                        Items = new List<ReceiptItem>()
-                           {
-                               new ReceiptItem()
-                               {
-                                    Title = "1大房",
-                                    Price = "90",
-                                    Quantity = "1",
-                                    Image = new CardImage("https://cdn.pixabay.com/photo/2014/08/11/21/40/wall-416062__180.jpg")
-                               },
-                               new ReceiptItem()
-                               {
-                                   Title = "飲料",
-                                   Price = "10",
-                                   Quantity = "1",
-                                   Image = new CardImage("https://cdn.pixabay.com/photo/2014/09/26/19/51/coca-cola-462776_1280.jpg")
-                               }
-                           }
-                    };
+                    //returnMessage.Attachments = new List<Attachment>() { receiptCard.ToAttachment() };
 
-                    returnMessage.Attachments = new List<Attachment>() { receiptCard.ToAttachment() };
-
-                    await context.PostAsync(returnMessage);
-                    context.Wait(MessageReceivedAsync);
+                    //await context.PostAsync(returnMessage);
+                    //context.Wait(MessageReceivedAsync);
                 }
                 else if(activity.Text == "查飯店v2")
                 {

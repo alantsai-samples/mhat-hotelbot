d33126f531e4af5ac0009f323905bfbd3f4d389e
Alan Tsai <alan@alantsai.net>
2018-07-14 23:17:17 +0800

    調整dialog邏輯

--- src/MHAT.HotelBot/Dialogs/ReserveRoomDialog.cs
+++ src/MHAT.HotelBot/Dialogs/ReserveRoomDialog.cs
@@ -1,5 +1,6 @@
 ﻿using MHAT.HotelBot.Models;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,45 @@ namespace MHAT.HotelBot.Dialogs
     {
         public Task StartAsync(IDialogContext context)
         {
-            throw new NotImplementedException();
+            var reserveRoomForm =
+                        FormDialog.FromForm(RoomReservation.BuildForm,
+                            FormOptions.PromptInStart);
+
+            context.Call(reserveRoomForm, AfterReserveRoomAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task AfterReserveRoomAsync(IDialogContext context
+           , IAwaitable<RoomReservation> result)
+        {
+            RoomReservation reservation = null;
+
+            try
+            {
+                reservation = await result;
+
+                //await context.PostAsync($"得到的結果：{Environment.NewLine} {JsonConvert.SerializeObject(reservation)}");
+            }
+            catch (FormCanceledException<RoomReservation> ex)
+            {
+                string reply;
+
+                if (ex.InnerException == null)
+                {
+                    reply = $"您在 {ex.Last} 的時候退出了 -- 如果有遇到任何問題請告訴我們";
+                }
+                else
+                {
+                    reply = "機器人暫時罷工了，請稍後嘗試";
+                }
+
+                await context.PostAsync(reply);
+            }
+            finally
+            {
+                context.Done(reservation);
+            }
         }
     }
 }
--- src/MHAT.HotelBot/Dialogs/RootDialog.cs
+++ src/MHAT.HotelBot/Dialogs/RootDialog.cs
@@ -58,11 +58,7 @@ namespace MHAT.HotelBot.Dialogs
                 }
                 else if(activity.Text == "訂房v2")
                 {
-                    var reserveRoomForm = 
-                        FormDialog.FromForm(RoomReservation.BuildForm,
-                            FormOptions.PromptInStart);
-
-                    context.Call(reserveRoomForm, AfterReserveRoomAsync);
+                    
                 }
                 else
                 {
@@ -84,38 +80,6 @@ namespace MHAT.HotelBot.Dialogs
             await context.PostAsync($"{name} 您好，能夠幫助您什麽");
 
             context.Wait(MessageReceivedAsync);
-        }
-
-        private async Task AfterReserveRoomAsync(IDialogContext context
-            , IAwaitable<RoomReservation> result)
-        {
-            RoomReservation reservation = null;
-
-            try
-            {
-                reservation = await result;
-
-                await context.PostAsync($"得到的結果：{Environment.NewLine} {JsonConvert.SerializeObject(reservation)}");
-            }
-            catch(FormCanceledException<RoomReservation> ex)
-            {
-                string reply;
-
-                if (ex.InnerException == null)
-                {
-                    reply = $"您在 {ex.Last} 的時候退出了 -- 如果有遇到任何問題請告訴我們";
-                }
-                else
-                {
-                    reply = "機器人暫時罷工了，請稍後嘗試";
-                }
-
-                await context.PostAsync(reply);
-            }
-            finally
-            {
-                context.Wait(MessageReceivedAsync);
-            }
         }
     }
 }

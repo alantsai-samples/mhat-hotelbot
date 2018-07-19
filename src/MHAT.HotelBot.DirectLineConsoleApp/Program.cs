5b2afd5aade834760ad55ccbace733faea97549a
Alan Tsai <alan@alantsai.net>
2018-07-19 20:17:07 +0800

    加入參數設定

--- src/MHAT.HotelBot.DirectLineConsoleApp/Program.cs
+++ src/MHAT.HotelBot.DirectLineConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,13 @@ namespace MHAT.HotelBot.DirectLineConsoleApp
 {
     class Program
     {
+        private static string directLineSecret = 
+            ConfigurationManager.AppSettings["DirectLineSecret"];
+
+        private static string botId = ConfigurationManager.AppSettings["BotId"];
+
+        private static string fromUser = "DirectLineSampleClientUser";
+
         static void Main(string[] args)
         {
         }

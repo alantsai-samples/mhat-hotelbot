d0d6ea1751cc8f3ec39fbe301207c8fb36113aa9
Alan Tsai <alan@alantsai.net>
2018-07-24 22:24:11 +0800

    加入RootLuisDialog

---
+++ src/MHAT.HotelBot/Dialogs/RootLuisDialog.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MHAT.HotelBot.Dialogs
+{
+    [LuisModel("<modelId>", "<key>")]
+    [Serializable]
+    public class RootLuisDialog : LuisDialog<object>
+    {
+    }
+}

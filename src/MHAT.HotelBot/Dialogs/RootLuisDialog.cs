e51013f7b03182c68226051f75043fb5e73b0d8e
Alan Tsai <alan@alantsai.net>
2018-07-24 22:50:17 +0800

    讓dialog處理對應的intent

--- src/MHAT.HotelBot/Dialogs/RootLuisDialog.cs
+++ src/MHAT.HotelBot/Dialogs/RootLuisDialog.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace MHAT.HotelBot.Dialogs
@@ -11,5 +14,32 @@ namespace MHAT.HotelBot.Dialogs
     [Serializable]
     public class RootLuisDialog : LuisDialog<object>
     {
+        [LuisIntent("")]
+        public async Task None
+            (IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("無法理解您的請求");
+
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("SearchHotel")]
+        public Task SearchHotel
+            (IDialogContext context, LuisResult result)
+        {
+            context.Call(new SearchHotelDialog(), async (ctx, r) =>
+            {
+                var imessageActivity = ctx.Activity as Activity;
+
+                var returnMessage = imessageActivity.CreateReply();
+                var attachments = await r;
+                returnMessage.Attachments = attachments;
+
+                await context.PostAsync(returnMessage);
+                context.Wait(MessageReceived);
+            });
+
+            return Task.CompletedTask;
+        }
     }
 }

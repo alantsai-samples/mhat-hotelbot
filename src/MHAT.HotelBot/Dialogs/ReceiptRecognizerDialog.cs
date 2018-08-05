184fd264664f3df15ca16f6c3ac03ea0ba6aaf59
Alan Tsai <alan@alantsai.net>
2018-08-05 13:09:20 +0800

    建立出Dialog處理對發票

---
+++ src/MHAT.HotelBot/Dialogs/ReceiptRecognizerDialog.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MHAT.HotelBot.Dialogs
+{
+    public class ReceiptRecognizerDialog : IDialog<string>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

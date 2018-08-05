ec130b2a0af2dad6385e2a7b525725e6ad8d8340
Alan Tsai <alan@alantsai.net>
2018-08-05 13:33:22 +0800

    加入OCRService

---
+++ src/MHAT.HotelBot/Services/OCRService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MHAT.HotelBot.Services
+{
+    public class OCRService
+    {
+        public OCRService()
+        {
+            VisionServiceClientInstance =
+                 new VisionServiceClient
+                    (ConfigurationManager.AppSettings["ComputerVision.Key"],
+                    ConfigurationManager.AppSettings["ComputerVision.Url"]);
+        }
+
+        public VisionServiceClient VisionServiceClientInstance { get; }
+
+        public async Task<OcrResults> GetOcrResultAsync
+            (Stream imageStream, string languageCode = "unk")
+        {
+            return await VisionServiceClientInstance.RecognizeTextAsync(imageStream, languageCode);
+        }
+
+        public async Task<OcrResults> GetOcrResultAsync
+            (string imageUrl, string languageCode = "unk")
+        {
+            return await VisionServiceClientInstance.RecognizeTextAsync
+                (imageUrl, languageCode);
+        }
+    }
+}

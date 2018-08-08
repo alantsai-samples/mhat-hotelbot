861a1602591a9fd990c82a9e2a57be754974e292
Alan Tsai <alan@alantsai.net>
2018-08-08 19:56:09 +0800

    建立一個Custom Vision的Service

---
+++ src/MHAT.HotelBot/Models/PredicationResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MHAT.HotelBot.Models
+{
+    public class PredicationResponse
+    {
+        public string id { get; set; }
+        public string project { get; set; }
+        public string iteration { get; set; }
+        public DateTime created { get; set; }
+        public Prediction[] predictions { get; set; }
+    }
+
+    public class Prediction
+    {
+        public float probability { get; set; }
+        public string tagId { get; set; }
+        public string tagName { get; set; }
+    }
+}
---
+++ src/MHAT.HotelBot/Services/CustomVisionService.cs
@@ -0,0 +1,116 @@
+﻿using MHAT.HotelBot.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MHAT.HotelBot.Services
+{
+    public class CustomVisionService
+    {
+        public string ProjectId { get; }
+        public string PredictionKey { get; }
+
+        public string PredictionBaseUrl
+        {
+            get
+            {
+                return $"https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/{ProjectId}";
+            }
+        }
+
+        public string PredictionImageUrl
+        {
+            get
+            {
+                return $"{PredictionBaseUrl}/image";
+            }
+        }
+
+        public string PredictionImageUrlUrl
+        {
+            get
+            {
+                return $"{PredictionBaseUrl}/url";
+            }
+        }
+
+        public CustomVisionService(string projectId, string predictionKey)
+        {
+            ProjectId = projectId;
+            PredictionKey = predictionKey;
+        }
+
+        public async Task<string> GetTag(string imageUrl)
+        {
+            var result = string.Empty;
+
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid subscription key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", PredictionKey);
+
+            // Prediction URL - replace this example URL with your valid prediction URL.
+            string url = PredictionImageUrlUrl;
+
+            HttpResponseMessage response;
+
+            using (var content = new StringContent($"{{ \"Url\": \"{imageUrl}\"}}"))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(url, content);
+                var json = await response.Content.ReadAsStringAsync();
+                result = GetMostPossibleTagName(json);
+            }
+
+            return result;
+        }
+
+        public async Task<string> GetTag(Stream stream)
+        {
+            var result = string.Empty;
+
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid subscription key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", PredictionKey);
+
+            // Prediction URL - replace this example URL with your valid prediction URL.
+            string url = PredictionImageUrl;
+
+            HttpResponseMessage response;
+
+            // Request body. Try this sample with a locally stored image.
+            byte[] byteData = GetStreamAsByteArray(stream);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                var json = await response.Content.ReadAsStringAsync();
+                result = GetMostPossibleTagName(json);
+            }
+
+            return result;
+        }
+
+        private string GetMostPossibleTagName(string json)
+        {
+            var model = JsonConvert.DeserializeObject<PredicationResponse>(json);
+
+            return $"{model.predictions.FirstOrDefault().tagName}";
+        }
+
+        private byte[] GetStreamAsByteArray(Stream stream)
+        {
+            var ms = new MemoryStream();
+            stream.CopyTo(ms);
+            return ms.ToArray();
+        }
+    }
+}

b36e489a5e3b917980f2d4029cdb862c4023b979
Alan Tsai <alan@alantsai.net>
2018-08-14 23:34:49 +0800

    加入呼叫Translator Speech Api的邏輯

--- src/MHAT.TranslatorSpeechQuickStart/Program.cs
+++ src/MHAT.TranslatorSpeechQuickStart/Program.cs
@@ -1,15 +1,125 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MHAT.TranslatorSpeechQuickStart
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static string host = "wss://dev.microsofttranslator.com";
+        static string path = "/speech/translate";
+
+        // TODO: 放入建立的Translator Speech Api
+        static string key = "";
+
+        async static Task Send(ClientWebSocket client, string input_path)
         {
+            var audio = File.ReadAllBytes(input_path);
+            var audio_out_buffer = new ArraySegment<byte>(audio);
+            Console.WriteLine("Sending audio.");
+            await client.SendAsync(audio_out_buffer, WebSocketMessageType.Binary, true, CancellationToken.None);
+
+            /* Make sure the audio file is followed by silence.
+             * This lets the service know that the audio input is finished. */
+            var silence = new byte[32000];
+            var silence_buffer = new ArraySegment<byte>(silence);
+            await client.SendAsync(silence_buffer, WebSocketMessageType.Binary, true, CancellationToken.None);
+
+            Console.WriteLine("Done sending.");
         }
+
+        async static Task<List<ResponseModel>> Receive(ClientWebSocket client, string output_path)
+        {
+            var outputResult = new List<ResponseModel>();
+
+            var inbuf = new byte[102400];
+            var segment = new ArraySegment<byte>(inbuf);
+            var stream = new FileStream(output_path, FileMode.Create);
+
+            Console.WriteLine("Awaiting response.");
+            while (client.State == WebSocketState.Open)
+            {
+                var result = await client.ReceiveAsync(segment, CancellationToken.None);
+                switch (result.MessageType)
+                {
+                    case WebSocketMessageType.Close:
+                        Console.WriteLine("Received close message. Status: " + result.CloseStatus + ". Description: " + result.CloseStatusDescription);
+                        await client.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                        break;
+                    case WebSocketMessageType.Text:
+                        Console.WriteLine("Received text.");
+                        var text = Encoding.UTF8.GetString(inbuf).TrimEnd('\0');
+                        Console.WriteLine(text);
+                        outputResult.Add(JsonConvert.DeserializeObject<ResponseModel>(text));
+                        if (result.EndOfMessage == true)
+                        {
+                            Console.WriteLine("Receive complete,closing socket.");
+                            await client.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                        }
+                        break;
+                    case WebSocketMessageType.Binary:
+                        Console.WriteLine("Received binary data: " + result.Count + " bytes.");
+                        stream.Write(inbuf, 0, result.Count);
+                        break;
+                }
+            }
+
+            stream.Close();
+            stream.Dispose();
+
+            return outputResult;
+        }
+
+        async static void TranslateSpeech()
+        {
+            var client = new ClientWebSocket();
+            client.Options.SetRequestHeader("Ocp-Apim-Subscription-Key", key);
+
+            string from = "en-US";
+            string to = "zh-Hant";
+            string voice = "it-IT-Elsa";
+            string api = "1.0";
+
+            string input_path = "speak.wav";
+            string output_path = "speak2.wav";
+
+            string uri = host + path +
+                "?from=" + from +
+                "&to=" + to +
+                "&api-version=" + api +
+                "&voice=" + voice;
+
+            Console.WriteLine("uri: " + uri);
+            Console.WriteLine("Opening connection.");
+            await client.ConnectAsync(new Uri(uri), CancellationToken.None);
+            Console.WriteLine("Connection open.");
+
+            var send = Send(client, input_path);
+            var receive = Receive(client, output_path);
+
+            await Task.WhenAll(send, receive);
+
+            var result = await receive;
+
+            Console.WriteLine("close connection");
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.recognition);
+            }
+        }
+
+        static void Main()
+        {
+            TranslateSpeech();
+            Console.ReadLine();
+        }
+
     }
 }

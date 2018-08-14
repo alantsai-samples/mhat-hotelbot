7192d9e1c1d021f29e3344b6c3125d0fcbc1a71f
Alan Tsai <alan@alantsai.net>
2018-08-14 23:32:17 +0800

    加入判斷的model

---
+++ src/MHAT.TranslatorSpeechQuickStart/ResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHAT.TranslatorSpeechQuickStart
+{
+    public class ResponseModel
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public string recognition { get; set; }
+        public string translation { get; set; }
+    }
+}
